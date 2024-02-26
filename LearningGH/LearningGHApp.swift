5454424fe24781a0b47ea38f771f70b28f46b989
mark bergeson <bergy450r@gmail.com>
2024-02-26 10:27:24 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Mark Bergeson on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
