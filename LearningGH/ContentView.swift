e5fb17ac43cc8226b7823a0fdc00dd8dda2afa82
Berg72 <bergy450r@gmail.com>
2024-08-27 19:31:42 -0700

    resize image

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "figure.wave")
+                .resizable()
+                .frame(width: 100.0, height: 150.0, alignment: .center)
             Text("What's up CodeCrew!")
         }
         .padding()
